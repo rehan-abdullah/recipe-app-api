6a09269798ff80b6e271d8888cfce6428e439ef8
rehan-abdullah <31924510+rehan-abdullah@users.noreply.github.com>
2020-03-23 15:33:44 +0000

    Added recipe model.

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 
+# noinspection PyUnresolvedReferences
 from core import models
 
 
@@ -28,3 +29,4 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Tag)
 admin.site.register(models.Ingredient)
+admin.site.register(models.Recipe)
