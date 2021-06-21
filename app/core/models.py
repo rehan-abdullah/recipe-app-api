7148bce0653f1feed8ce79389b4e66c89b1da3c4
rehan-abdullah <31924510+rehan-abdullah@users.noreply.github.com>
2021-06-21 13:51:10 +0100

    Added pipenv. Created core app and custom user model with TDD.

---
+++ app/core/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser,
+                                        BaseUserManager,
+                                        PermissionsMixin)
+
+
+class UserManager(BaseUserManager):
+
+    # **extra_fields allows any fields added to the model to be passed
+    # down to the manager.
+    def create_user(self, email, password=None, **extra_fields):
+        """ Creates and saves a new user """
+
+        # Raise ValueError if no email provided
+        # when trying to create a new user
+        if not email:
+            raise ValueError('Users must have an email address!')
+
+        # self.model allows the manager to access the model that the
+        # manager is targeting.
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        # using=self._db is required for supporting multiple databases
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """ Creates and saves a new superuser """
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        # user has been modified so must save
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """ Custom user model that supports using email instead of username. """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,53 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """ Test creating a new user with an email was successful. """
+
+        # Test setup
+        email = 'new_user@django.com'
+        password = 'testing321'
+
+        # Call the create_user function that will be implemented in
+        # Model manager for the custom user model
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        # Run test
+        self.assertEqual(user.email, email)
+
+        # Passwords must be checked using the check_password function
+        # as they are not stored in raw format.
+        # check_password is a helper function that comes with
+        # the Django user model.
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Test the email for a new user is normalized """
+        email = 'new_user@DJANGO.COM'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='testing321'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ Test creating user with no email raises error. """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'testing321')
+
+    def test_create_new_superuser(self):
+        """ Test that a new superuser was created. """
+
+        user = get_user_model().objects.create_superuser(
+            'admin@djano.com',
+            'testing321'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
